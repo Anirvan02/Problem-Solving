0ceb32c6eb69d433a755fafc3f8a6dd2434a8ded
Anirvan Pandey <130135426+Anirvan02@users.noreply.github.com>
2024-06-26 23:18:19 +0530

    Create Add Binary.java

---
+++ Add Binary.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger n1 = new BigInteger(a, 2);
+        BigInteger n2 = new BigInteger(b, 2);
+
+        BigInteger sum = n1.add(n2);
+
+        return sum.toString(2);
+    }
+}
